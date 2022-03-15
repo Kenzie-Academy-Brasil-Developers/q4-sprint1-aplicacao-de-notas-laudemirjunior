c7599f91161331351ce7c455a4e2d6584294d4d4
Laudemir <nlaudemir@gmail.com>
2022-03-15 20:42:00 -0300

    feat: finish aplication

--- index.js
+++ index.js
@@ -6,7 +6,33 @@ const PORT = 3000;
 
 app.use(express.json());
 
-let users = [];
+let users = [
+  {
+    id: 'fd218ada-ee7e-4b89-a3c2-2b65c53ef794',
+    name: 'laudemir',
+    cpf: '09772380919',
+    notes: [
+      {
+        id: '122ad11a-74a8-4e55-ba72-05baae49d92e',
+        title: 'Dica',
+        content: 'Organizar meu dia',
+        created_at: '2022-03-15T23:32:35.376Z',
+      },
+      {
+        id: '7276707c-d99d-40bf-bdd3-a9ce63e7eb01',
+        title: 'laudemir',
+        content: '09772380919',
+        created_at: '2022-03-15T23:34:43.147Z',
+      },
+      {
+        id: 'ce349aad-07df-4462-be26-59900ec3f763',
+        title: 'laudemir',
+        content: '09772380919',
+        created_at: '2022-03-15T23:35:45.863Z',
+      },
+    ],
+  },
+];
 
 const verifyIdParams = (req, res, next) => {
   const { cpf } = req.params;
@@ -47,7 +73,7 @@ app.post('/users', verifyIdBody, (req, res) => {
 });
 
 app.get('/users', (_, res) => {
-  res.json(users);
+  res.status(201).json(users);
 });
 
 app.patch('/users/:cpf', verifyIdParams, (req, res) => {
@@ -57,14 +83,14 @@ app.patch('/users/:cpf', verifyIdParams, (req, res) => {
   oldUser[0].name = name;
   oldUser[0].cpf = cpf;
   const user = oldUser[0];
-  res.json({ message: 'User is updated', user });
+  res.status(200).json({ message: 'User is updated', user });
 });
 
 app.delete('/users/:cpf', verifyIdParams, (req, res) => {
   const paramsCpf = req.params;
   const oldUser = users.filter((element) => paramsCpf === element.cpf);
   users = oldUser;
-  res.json();
+  res.json(204).json();
 });
 
 app.post('/users/:cpf/notes', verifyIdParams, (req, res) => {
@@ -77,7 +103,9 @@ app.post('/users/:cpf/notes', verifyIdParams, (req, res) => {
   // eslint-disable-next-line camelcase
   const newNote = { id, title, content, created_at };
   user[0].notes.push(newNote);
-  res.json({ message: `Dica was added into ${user[0].name} notes` });
+  res
+    .status(201)
+    .json({ message: `Dica was added into ${user[0].name} notes` });
 });
 
 app.get('/users/:cpf/notes', (req, res) => {
@@ -89,22 +117,22 @@ app.get('/users/:cpf/notes', (req, res) => {
 app.patch('/users/:cpf/notes/:id', verifyIdNotes, (req, res) => {
   const { cpf, id } = req.params;
   const { title, content } = req.body;
-  const user = users.filter((element) => cpf !== element.cpf);
+  const user = users.filter((element) => cpf === element.cpf);
   const notes = user[0].notes.filter((element) => id === element.id);
   // eslint-disable-next-line camelcase
   notes[0].title = title;
   notes[0].content = content;
   // eslint-disable-next-line camelcase
   notes[0].updated_at = new Date();
-  res.json(notes);
+  res.status(201).json(notes);
 });
 
 app.delete('/users/:cpf/notes/:id', verifyIdNotes, (req, res) => {
   const { cpf, id } = req.params;
-  const user = users.filter((element) => cpf !== element.cpf);
-  const notes = user.note.filter((element) => id !== element.id);
+  const user = users.filter((element) => cpf === element.cpf);
+  const notes = user[0].notes.filter((element) => id !== element.id);
   user[0].notes = notes;
-  res.json();
+  res.status(204).json();
 });
 
 app.listen(PORT, () =>
